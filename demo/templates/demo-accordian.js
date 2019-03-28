3435ab1fe652b441816bd87b431c5dd7d96c698d
Bhagyalakshmi M <bhagyalakshmi.m@infosys.com>
2019-03-28 13:48:30 +0530

    removed rotate function

--- demo/templates/demo-accordian.js
+++ demo/templates/demo-accordian.js
@@ -24,16 +24,10 @@ class DemoAccordian extends OECommonMixin(PolymerElement){
 
         </style>
         <div id="demo-accordian" class=" background-color" style="height: 100%;">
-
             <div>
-
                 <img id="cheque_image" on-load="imgFun" src="https://www.oecloud.io/images/logo.png" style="display: inline-block; width: 30%; margin: 20px;"
                 />
-
             </div>
-
-            <iron-icon icon="icons:zoom-in" item-icon on-tap="clickMe" style=" margin-left: 40px;"></iron-icon>
-            <iron-icon icon="icons:refresh" item-icon on-tap="rotate" style=" margin-left: 10px;"></iron-icon>
             <div>Account : {{data.account}}</div>
 
         </div>
@@ -59,11 +53,6 @@ class DemoAccordian extends OECommonMixin(PolymerElement){
     static get is() {
         return "demo-accordian";
     }
-
-    connectedCallback() {      
-        super.connectedCallback();
-                 
-    }   
     dataChanged(e) {
 
         console.log("pulkit");
@@ -72,19 +61,7 @@ class DemoAccordian extends OECommonMixin(PolymerElement){
     imgFun(){
         this.fire('expanded-view');
     }
-    rotate(e) {
-
-        var degrees = this.degrees;
-        var $cboxphoto = $('#cheque_image');
-        degrees += 90;
-        $cboxphoto.css('-ms-transform', 'rotate(' + degrees + 'deg)');
-        $cboxphoto.css('-webkit-transform', 'rotate(' + degrees + 'deg)');
-        $cboxphoto.css('transform', 'rotate(' + degrees + 'deg)');
-
-        this.degrees = degrees;
-
-
-    }
+   
 }
 
 window.customElements.define(DemoAccordian.is, DemoAccordian);
